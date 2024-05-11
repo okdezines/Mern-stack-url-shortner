72a244c15f999905cb7e556719faec0c1e38247d
ompa <nmaladina@hotmail.com>
2024-05-12 04:58:38 +1200

    added middleware and routes, controllers, and model for schema with functions

---
+++ server-app/src/controllers/shortlUrl.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { urlModel } from "../model/shortUrl";
+
+export const createUrl = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    console.log("The full Url is ", req.body.fullUrl);
+    const { fullUrl } = req.body;
+    const urlFound = await urlModel.find({ fullUrl });
+    if (urlFound.length > 0) {
+      res.status(409);
+      res.send(urlFound);
+    } else {
+      const shortUrl = await urlModel.create({ fullUrl });
+      res.status(201).send(shortUrl);
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong" });
+  }
+};
+export const getAllUrl = async (
+  req: express.Request,
+  res: express.Response
+) => {};
+export const getUrl = async (req: express.Request, res: express.Response) => {};
+export const deleteUrl = async (
+  req: express.Request,
+  res: express.Response
+) => {};
--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -2,17 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/dbConfig";
+import shortUrl from "./routes/shortUrl";
+
 dotenv.config();
 connectDb();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
+app.use("/api/", shortUrl);
 app.listen(port, () => {
   console.log(`Server started successfully on port : ${port}`);
 });
