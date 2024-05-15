9e0c6012c0eebdc2c535139138070fd6f188111b
ompa <nmaladina@hotmail.com>
2024-05-15 23:15:37 +1200

    Added components folder and created Header and Footer files

--- client-app/Url-shortner-app/src/App.tsx
+++ client-app/Url-shortner-app/src/App.tsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import * as React from "react";
+
+interface IAppProps {}
+
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
